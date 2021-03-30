4988cfc1aa637ccd14d000e7f428b5b565be0304
appcoreopc <jeremy@jeremys-MacBook-Pro.local>
2021-03-30 22:19:00 +0800

    added the required library

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-import fs from "fs";
+import { DefaultAzureCredential } from "@azure/identity";
+import { QueueServiceClient }   from "@azure/storage-queue";
+import { MessageSender } from "./messaging/messageSender";
+import { setLogLevel } from "@azure/logger";
 
-import { QueueServiceClient, StorageSharedKeyCredential }   from "@azure/storage-queue";
+const account = "thermoappsadev";
+const credential = new DefaultAzureCredential();
+const queueName = "testmessage";
+
+const queueServiceClient = new QueueServiceClient(`https://${account}.queue.core.windows.net`,
+  credential
+);
+
+setLogLevel("info");
+
+// scheduling starts here //
+
+const sender =  new MessageSender(queueServiceClient, queueName);
