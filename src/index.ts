291463bf2cdff31d41f0f52e57ecd2170e8bb167
appcoreopc <jeremy@jeremys-MacBook-Pro.local>
2021-03-30 22:47:45 +0800

    fix up code and refactor main

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,3 @@
-import { DefaultAzureCredential } from "@azure/identity";
-import { QueueServiceClient }   from "@azure/storage-queue";
-import { MessageSender } from "./messaging/messageSender";
-import { setLogLevel } from "@azure/logger";
+import { main }  from "./main";
 
-const account = "thermoappsadev";
-const credential = new DefaultAzureCredential();
-const queueName = "testmessage";
-
-const queueServiceClient = new QueueServiceClient(`https://${account}.queue.core.windows.net`,
-  credential
-);
-
-setLogLevel("info");
-
-// scheduling starts here //
-
-const sender =  new MessageSender(queueServiceClient, queueName);
+main();
